dec93ec495fc10ae0aa8bd66a740e79dae478259
Alexander <gamer34545@mail.ru>
2025-04-28 18:00:54 +0300

    npm init, add html

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  // Режим сборки: development или production
+  mode: 'development', // или 'production' для production-сборки
+
+  // Входная точка вашего приложения
+  entry: './src/index.js',
+
+  // Выходные настройки
+  output: {
+    filename: 'bundle.js', // Имя выходного JavaScript-файла
+    path: path.resolve(__dirname, 'dist'), // Путь к выходной директории
+    clean: true, // Очищать выходную директорию перед каждой сборкой
+  },
+
+  // Настройки devServer для разработки
+  devServer: {
+    static: './dist', // Корневая директория для статического контента
+    port: 8080, // Порт devServer
+    open: true, // Автоматически открывать браузер после запуска
+  },
+
+  // Модули (loaders) для обработки различных типов файлов
+  module: {
+    rules: [
+      // Обработка JavaScript файлов
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'], // Если используете React
+          },
+        },
+      },
+      // Обработка SCSS файлов
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extract CSS to separate files
+          'css-loader', // Translates CSS into CommonJS
+          'sass-loader', // Compiles Sass to CSS
+        ],
+      },
+      // Обработка CSS файлов (если есть отдельные CSS)
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      // Обработка HTML файлов
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      // Обработка изображений и других статических активов (опционально)
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name].[ext]', // Путь и имя для выходных файлов
+        },
+      },
+      // Обработка шрифтов (опционально)
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name].[ext]',
+        },
+      },
+    ],
+  },
+
+  // Плагины
+  plugins: [
+    // Плагин для генерации HTML файла
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Путь к вашему HTML шаблону
+      filename: 'index.html', // Имя выходного HTML файла
+    }),
+    // Плагин для извлечения CSS в отдельные файлы
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Имя выходного CSS файла
+    }),
+  ],
+
+  // Настройки для source maps (для облегчения отладки)
+  devtool: 'inline-source-map', // Рекомендуется для разработки
+};
