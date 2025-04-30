658806bf3316462a0d9c3a08532b66dca54a7c33
Alexander <gamer34545@mail.ru>
2025-04-30 13:29:14 +0300

    add mobile menus

--- webpack.config.js
+++ webpack.config.js
@@ -91,6 +91,14 @@ module.exports = {
       template: './src/mobile-menu-2lvl.html', // Путь к вашему HTML шаблону
       filename: 'mobile-menu-2lvl.html', // Имя выходного HTML файла
     }),
+    new HtmlWebpackPlugin({
+      template: './src/mobile-menu-3lvl.html', // Путь к вашему HTML шаблону
+      filename: 'mobile-menu-3lvl.html', // Имя выходного HTML файла
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/mobile-menu-4lvl.html', // Путь к вашему HTML шаблону
+      filename: 'mobile-menu-4lvl.html', // Имя выходного HTML файла
+    }),
     // Плагин для извлечения CSS в отдельные файлы
     new MiniCssExtractPlugin({
       filename: 'styles.css', // Имя выходного CSS файла
